48757c7bd7d6b8fddb1d86b53663a7a15d0f5d1e
Lucine0508 <113482570+Lucine0508@users.noreply.github.com>
2023-04-26 02:46:42 -0400

    5

--- main.js
+++ main.js
@@ -4,11 +4,13 @@
 let AM = [{"src":"https://d2w9rnfcy7mm78.cloudfront.net/21557105/original_599f11a168c54351eeb13132553c5c3e.png?1682485483?bc=0"},
     {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557106/original_3ba10bd0dea3e51ba2a088e24c0bf89d.png?1682485485?bc=0"},
     {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557107/original_96c51c3ef22576718436291d84de34f6.png?1682485486?bc=0"},
+    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557521/original_e443879436be6b875cbe996b6a9d0b02.png?1682488991?bc=0"},
     {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557122/original_b70c73b2c0cf6caee9b4cfd3c9216f18.png?1682485625?bc=0"}];
 let PM = [{"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557112/original_833c9d3cc2adbb2b6340813cbc33a286.png?1682485517?bc=0"},
     {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557114/original_a9745e1933dac2b69644b8e51b8c8a52.png?1682485518?bc=0"},
-    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557070/original_8cd2b8e43551fcffa582f0afc10e6f36.png?1682485063?bc=0"},
-    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557071/original_2bc4316eabd216f5f345053403215385.png?1682485065?bc=0"},];
+    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557723/original_baada66b7364319b8c7c0cb9ba973bd4.png?1682490275?bc=0"},
+    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557875/original_901cb56f740984a9be9bb0a9b76caf69.png?1682491317?bc=0"},
+    {"src": "https://d2w9rnfcy7mm78.cloudfront.net/21557763/original_9030244b951d7838aa3159921e77c21b.png?1682490713?bc=0"},];
 // 0 off 1 on
 let state = 0;
 // ampm, 0am 1pm
